fe9b08defad9e56334b9f7da117deff5b4a82efe
tomtch <tomtch7@gmail.com>
2023-11-30 12:12:44 +0100

    fixed loading weights

--- WalkerPolicy.py
+++ WalkerPolicy.py
@@ -3,12 +3,15 @@ from torch import nn
 
 class WalkerPolicy(nn.Module):
     def __init__(self, state_dim=21, action_dim=4):
-        self.load_weights()  # call learned stored network weights
+        super().__init__()
+        # self.load_weights()  # load learned stored network weights after initialization
 
     # TODO: implement a determine_actions() function mapping from (N, state_dim) states into (N, action_dim) actions
 
-    def save_weights(self, path='weights_path.t'):
+    def save_weights(self, path='walker_weights.pt'):
+        # helper function to save your network weights
         torch.save(self.state_dict(), path)
 
-    def load_weights(self, path='weights_path.t'):
+    def load_weights(self, path='walker_weights.pt'):
+        # helper function to load your network weights
         self.load_state_dict(torch.load(path))
